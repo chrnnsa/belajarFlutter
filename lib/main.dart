85cb119a06ab3676967ab7aa5ed84647d3fc3454
chairun089@gmail.com <chairun089@gmail.com>
2021-10-07 15:17:27 +0700

    first commit : tugas kdua

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,9 @@ class Home extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightBlueAccent,
+        backgroundColor: Colors.blueGrey,
         title: Text('Flutter App', style: TextStyle(
-          color: Colors.purple,
+          color: Colors.white,
           fontWeight: FontWeight.bold
         ),
         ),
@@ -29,6 +29,16 @@ class Home extends StatelessWidget{
           Icon(Icons.account_circle), Icon(Icons.access_alarm),
         ],
         centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.blueGrey,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        padding: EdgeInsets.all(30),
+        child: Icon(
+          Icons.home,
+          color: Colors.white,
+          size: 100.78,
+        ),
       ),
     );
   }
